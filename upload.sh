136c19d11bcbbbf585b8851941bccfc1ac68b04a
SimKDT <simon.cadet79@hotmail.fr>
2026-04-27 23:39:49 +0200

    update upload script to SteamUploader-rs

--- upload.sh
+++ upload.sh
@@ -4,30 +4,11 @@ set -e
 VERSION="${1:?Usage: ./upload.sh <version> [gh-options]}"
 shift || true
 
-# make sure main branch is being uploaded
-git checkout main
-git pull
-
-# store current folder
-WORKSHOP_DIR=$(pwd)
-
-MOD_TITLE="Horse Mod [B42.14+/MP SOON]"
-WORKSHOP_ID=3661336777
-VISIBILITY=0
-TAGS="Build 42,Animals,Items,Misc,Vehicles,Models"
-
 # need to be in the steam uploader folder
-cd "$STEAMUPLOADER"
-./SteamUploader --appID 108600 --workshopID "$WORKSHOP_ID" \
-    --description "$WORKSHOP_DIR/Steam/description.bbcode" \
-    --patchNote "$WORKSHOP_DIR/Steam/patch_notes/$VERSION.bbcode" \
-    -c "$WORKSHOP_DIR/Contents" \
-    --preview "$WORKSHOP_DIR/Steam/preview.gif" \
-    --title "$MOD_TITLE" --visibility "$VISIBILITY" --tags "$TAGS"
+SteamUploader upload --patchnote "$WORKSHOP_DIR/Steam/patch_notes/$VERSION.bbcode"
 
-# go to the contents folder and zip it for github release
-cd "$WORKSHOP_DIR/Contents"
-
+# github release
+cd Contents
 ARCHIVE="/tmp/release.zip"
 zip -r "$ARCHIVE" mods
 
